0b89c0c3d10c8911faefacdb62c6b06a508a2198
unknown <jefferson.crn@gmail.com>
2024-06-04 15:49:16 -0300

    style: align form-result and change color msg err

--- src/components/NotFoundUser/index.tsx
+++ src/components/NotFoundUser/index.tsx
@@ -4,7 +4,7 @@ export default function NotFoundUser() {
     return (
         <>
             <div className="gap-container-err-msg">
-                <h1 className="gap-err-user">Erro ao buscar usuário</h1>
+                <h1 className="gap-err-user">usuário inexistente!</h1>
             </div>
         </>
     );
